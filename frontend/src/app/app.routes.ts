5a65ce8c582a7e49c3adb71634f6df01ebdaf4da
Saumya Verma <vsaumyav20@gmail.com>
2026-05-27 22:25:08 +0530

    Initialized Angular frontend application with routing and basic components

---
+++ frontend/src/app/app.routes.ts
@@ -0,0 +1,41 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./features/auth/pages/login/login.component')
+        .then(m => m.LoginComponent)
+  },
+  {
+    path: '',
+    loadComponent: () =>
+      import('./shared/layouts/main-layout/main-layout.component')
+        .then(m => m.MainLayoutComponent),
+    children: [
+      {
+        path: 'dashboard',
+        loadComponent: () =>
+          import('./features/dashboard/pages/dashboard/dashboard.component')
+            .then(m => m.DashboardComponent)
+      },
+      {
+        path: 'incidents',
+        loadComponent: () =>
+          import('./features/incidents/pages/incidents/incidents.component')
+            .then(m => m.IncidentsComponent)
+      },
+      {
+        path: 'logs/upload',
+        loadComponent: () =>
+          import('./features/logs/pages/upload-logs/upload-logs.component')
+            .then(m => m.UploadLogsComponent)
+      },
+      {
+        path: '',
+        redirectTo: 'dashboard',
+        pathMatch: 'full'
+      }
+    ]
+  }
+];
---
+++ frontend/src/app/features/logs/pages/upload-logs/upload-logs.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-upload-logs',
+  standalone: true,
+  imports: [],
+  templateUrl: './upload-logs.component.html',
+  styleUrl: './upload-logs.component.scss'
+})
+export class UploadLogsComponent {
+
+}
